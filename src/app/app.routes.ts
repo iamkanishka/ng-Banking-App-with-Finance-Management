cd7ff22dc676a51d8cdb2174035ed38060b84a28
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-18 20:24:03 +0530

    chore: Added Auth Routes to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from '../components/auth-layout/auth-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('../pages/auth/auth-routes').then((r) => r.AUTH_ROUTES),
+      },
+    ],
+  },
+];
