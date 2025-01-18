62c981ac29399c1652df492700789af00a07b239
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-18 19:53:44 +0530

    chore: transaction-history page  initilal commit

---
+++ src/pages/transaction-history-page/transaction-history-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-history-page',
+  imports: [],
+  templateUrl: './transaction-history-page.component.html',
+  styleUrl: './transaction-history-page.component.scss'
+})
+export class TransactionHistoryPageComponent {
+
+}
