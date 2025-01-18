8ba4daf4e735e9a267d26d2fbab8c332b4ce4295
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-18 20:19:32 +0530

    feat: Added  signup route

--- src/pages/auth/auth-routes.ts
+++ src/pages/auth/auth-routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { SigninPageComponent } from './signin-page/signin-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 export const AUTH_ROUTES: Routes = [
   {
     path: '',
     // canActivate: [NoUserLoggedGuard],
     component: SigninPageComponent,
+  },
+  {
+    path: 'sign-up',
+    // canActivate: [NoUserLoggedGuard],
+    component: SignupPageComponent,
   },
 ];
