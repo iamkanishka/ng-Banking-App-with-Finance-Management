9a0f114d7d06a99ca16586ae9f8d35bd53473ddd
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-19 21:37:27 +0530

    chore: Plaid-Link comp Initila commit

---
+++ src/components/plaid-link/plaid-link.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plaid-link',
+  imports: [],
+  templateUrl: './plaid-link.component.html',
+  styleUrl: './plaid-link.component.scss'
+})
+export class PlaidLinkComponent {
+
+}
