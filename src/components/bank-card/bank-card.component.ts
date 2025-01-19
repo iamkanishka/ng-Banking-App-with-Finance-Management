b2aa4f2e1998b606347710ab2f8454c5faead395
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-19 21:38:20 +0530

    chore: bank-card comp Initial commit

---
+++ src/components/bank-card/bank-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-card',
+  imports: [],
+  templateUrl: './bank-card.component.html',
+  styleUrl: './bank-card.component.scss'
+})
+export class BankCardComponent {
+
+}
