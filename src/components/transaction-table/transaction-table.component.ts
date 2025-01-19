15c05b6888a430266a5a3606e3a7573c29a5175c
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-19 21:38:53 +0530

    chore: Transaction table comp Initial commit

---
+++ src/components/transaction-table/transaction-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-table',
+  imports: [],
+  templateUrl: './transaction-table.component.html',
+  styleUrl: './transaction-table.component.scss'
+})
+export class TransactionTableComponent {
+
+}
