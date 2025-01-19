400913a74d969df80b571d5584ccf7fb2057a808
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-19 21:40:19 +0530

    chore: header-box comp Initial commit

---
+++ src/components/header-box/header-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-box',
+  imports: [],
+  templateUrl: './header-box.component.html',
+  styleUrl: './header-box.component.scss'
+})
+export class HeaderBoxComponent {
+
+}
