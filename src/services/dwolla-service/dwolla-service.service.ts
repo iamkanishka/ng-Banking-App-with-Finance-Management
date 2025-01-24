d4a46a202aa0b7356cec9e2da41b2a97f801d317
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-24 15:14:52 +0530

    feat: Added Created funding source dn

--- src/services/dwolla-service/dwolla-service.service.ts
+++ src/services/dwolla-service/dwolla-service.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
 
+import { Client } from 'dwolla-v2';
+import { environment } from '../../environments/environment.development';
+import { CreateFundingSourceOptions } from '../../types/types';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DwollaServiceService {
+  dwollaClient: Client = new Client({
+    environment: 'sandbox',
+    key: environment.DWOLLA_KEY as string,
+    secret: environment.DWOLLA_SECRET as string,
+  });
+  constructor() {}
 
-  constructor() { }
+  // Create a Dwolla Funding Source using a Plaid Processor Token
+  async createFundingSource(
+    options: CreateFundingSourceOptions
+  ): Promise<string | null | undefined> {
+    try {
+      return await this.dwollaClient
+        .post(`customers/${options.customerId}/funding-sources`, {
+          name: options.fundingSourceName,
+          plaidToken: options.plaidToken,
+        })
+        .then((res) => res.headers.get('location'));
+    } catch (err) {
+      console.error('Creating a Funding Source Failed: ', err);
+      return null;
+    }
+  }
 }
