9296f4451653178f52f453571b8c126ac818d8f2
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-24 17:22:22 +0530

    chore: Added PLAID_CLIENT_ID and PLAID_SECRET

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: false,
   DWOLLA_KEY: '',
   DWOLLA_SECRET: '',
+  PLAID_CLIENT_ID: '',
+  PLAID_SECRET: '',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,7 @@ export const environment = {
   production: true,
   DWOLLA_KEY: '',
   DWOLLA_SECRET: '',
+  PLAID_CLIENT_ID: '',
+  PLAID_SECRET: '',
+
 };
