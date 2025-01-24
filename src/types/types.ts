ff7943dc5d70cfc7ea23f5dc96b50f80da39c2e7
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-24 15:58:39 +0530

    feat: NewDwollaCustomerParams type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,19 @@
-export  interface CreateFundingSourceOptions {
-    customerId: string; // Dwolla Customer ID
-    fundingSourceName: string; // Dwolla Funding Source Name
-    plaidToken: string; // Plaid Account Processor Token
-    _links: object; // Dwolla On Demand Authorization Link
-  }
-  
+export interface CreateFundingSourceOptions {
+  customerId: string; // Dwolla Customer ID
+  fundingSourceName: string; // Dwolla Funding Source Name
+  plaidToken: string; // Plaid Account Processor Token
+  _links: object; // Dwolla On Demand Authorization Link
+}
+
+export type NewDwollaCustomerParams = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  type: string;
+  address1: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  dateOfBirth: string;
+  ssn: string;
+};
