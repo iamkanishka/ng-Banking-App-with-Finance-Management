7ea08c64fb61186fc86880c03126777d627c1bb1
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-24 17:22:40 +0530

    feat: Plaidclient

--- src/services/plaid-service/plaid-service.service.ts
+++ src/services/plaid-service/plaid-service.service.ts
@@ -1,9 +1,22 @@
+import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
+import { environment } from '../../environments/environment.development';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PlaidServiceService {
+  configuration = new Configuration({
+    basePath: 'sandbox',
+    baseOptions: {
+      headers: {
+        'PLAID-CLIENT-ID': environment.PLAID_CLIENT_ID,
+        'PLAID-SECRET': environment.PLAID_SECRET,
+      },
+    },
+  });
 
-  constructor() { }
+  plaidClient = new PlaidApi(this.configuration);
+
+  constructor() {}
 }
