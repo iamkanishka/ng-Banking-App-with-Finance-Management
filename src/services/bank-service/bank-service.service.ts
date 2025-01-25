0d4afaa8c6f12aed45d82c1819c3d404e0cea135
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-25 22:57:11 +0530

    chore: getBank  fn

--- src/services/bank-service/bank-service.service.ts
+++ src/services/bank-service/bank-service.service.ts
@@ -3,36 +3,51 @@ import { environment } from '../../environments/environment.development';
 import { AppwriteService } from '../appwrite/appwrite.service';
 import { PlaidServiceService } from '../plaid-service/plaid-service.service';
 import { parseStringify } from '../../utils/util';
-import { getBanks } from '../../types/types';
+import { getBank, getBanks } from '../../types/types';
 import { DwollaServiceService } from '../dwolla-service/dwolla-service.service';
 import { Query } from 'appwrite';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BankServiceService {
+  DATABASE_ID: string = environment.APPWRITE_DATABASE_ID;
+  BANK_COLLECTION_ID: string = environment.APPWRITE_BANK_COLLECTION_ID;
 
-    DATABASE_ID: string = environment.APPWRITE_DATABASE_ID;
-    BANK_COLLECTION_ID: string = environment.APPWRITE_BANK_COLLECTION_ID;
+  constructor(
+    private appwriteService: AppwriteService,
+    private plaidService: PlaidServiceService,
+    private dwollaService: DwollaServiceService
+  ) {}
 
-  constructor(  private appwriteService: AppwriteService,
-      private plaidService: PlaidServiceService,
-      private dwollaService: DwollaServiceService) { }
-
-  async getBanks({ userId }: getBanks){
+  async getBanks({ userId }: getBanks) {
     try {
       const { database } = await this.appwriteService.createAdminClient();
-  
+
       const banks = await database.listDocuments(
         this.DATABASE_ID!,
         this.BANK_COLLECTION_ID!,
-        [Query.equal("userId", [userId])]
+        [Query.equal('userId', [userId])]
       );
-  
+
       return parseStringify(banks.documents);
     } catch (error) {
       console.log(error);
     }
-  };
+  }
 
+  async getBank({ documentId }: getBank) {
+    try {
+      const { database } = await this.appwriteService.createAdminClient();
+      const bank = await database.listDocuments(
+        this.DATABASE_ID!,
+        this.BANK_COLLECTION_ID!,
+        [Query.equal('$id', [documentId])]
+      );
+
+      return parseStringify(bank.documents[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
