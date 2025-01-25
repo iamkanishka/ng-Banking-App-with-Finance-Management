d8115e54b836e8fbcc71d9baae72c1ba305cec1c
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-25 11:33:13 +0530

    chore: Signup fn

--- src/services/user-service/user.service.ts
+++ src/services/user-service/user.service.ts
@@ -11,8 +11,8 @@ import {
 } from 'plaid';
 import { DwollaServiceService } from '../dwolla-service/dwolla-service.service';
 import { environment } from '../../environments/environment.development';
-import { getUserInfo, signIn } from '../../types/types';
-import { parseStringify } from '../../utils/util';
+import { getUserInfo, signIn, SignUp } from '../../types/types';
+import { parseStringify, extractCustomerIdFromUrl } from '../../utils/util';
 @Injectable({
   providedIn: 'root',
 })
@@ -43,24 +43,77 @@ export class UserService {
     }
   }
 
-  async signIn ({ email, password }: signIn) {
+  async signIn({ email, password }: signIn) {
     try {
       const { account } = await this.appwriteService.createAdminClient();
       const session = await account.createEmailPasswordSession(email, password);
-  
+
       // cookies().set("appwrite-session", session.secret, {
       //   path: "/",
       //   httpOnly: true,
       //   sameSite: "strict",
       //   secure: true,
       // });
-  
+
       const user = await this.getUserInfo({ userId: session.userId });
-  
+
       return parseStringify(user);
     } catch (error) {
-      console.error("Error", error);
+      console.error('Error', error);
     }
-  };
+  }
 
+  async signUp({ password, ...userData }: SignUp) {
+    const { email, firstName, lastName } = userData;
+
+    let newUserAccount;
+
+    try {
+      const { account, database } =
+        await this.appwriteService.createAdminClient();
+
+      newUserAccount = await account.create(
+        ID.unique(),
+        email,
+        password,
+        `${firstName} ${lastName}`
+      );
+
+      if (!newUserAccount) throw new Error('Error creating user');
+
+      const dwollaCustomerUrl = await this.dwollaService.createDwollaCustomer({
+        ...userData,
+        type: 'personal',
+      });
+
+      if (!dwollaCustomerUrl) throw new Error('Error creating Dwolla customer');
+
+      const dwollaCustomerId = extractCustomerIdFromUrl(dwollaCustomerUrl);
+
+      const newUser = await database.createDocument(
+        this.DATABASE_ID!,
+        this.USER_COLLECTION_ID!,
+        ID.unique(),
+        {
+          ...userData,
+          userId: newUserAccount.$id,
+          dwollaCustomerId,
+          dwollaCustomerUrl,
+        }
+      );
+
+      const session = await account.createEmailPasswordSession(email, password);
+
+      // cookies().set("appwrite-session", session.secret, {
+      //   path: "/",
+      //   httpOnly: true,
+      //   sameSite: "strict",
+      //   secure: true,
+      // });
+
+      return parseStringify(newUser);
+    } catch (error) {
+      console.error('Error', error);
+    }
+  }
 }
