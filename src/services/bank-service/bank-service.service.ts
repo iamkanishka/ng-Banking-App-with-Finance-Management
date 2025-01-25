a5f88ecd55cc3bdb7d0371ac5bf6f47978ac2626
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-25 22:51:23 +0530

    chore: getBanks  fn

--- src/services/bank-service/bank-service.service.ts
+++ src/services/bank-service/bank-service.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { AppwriteService } from '../appwrite/appwrite.service';
+import { PlaidServiceService } from '../plaid-service/plaid-service.service';
+import { parseStringify } from '../../utils/util';
+import { getBanks } from '../../types/types';
+import { DwollaServiceService } from '../dwolla-service/dwolla-service.service';
+import { Query } from 'appwrite';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BankServiceService {
 
-  constructor() { }
+    DATABASE_ID: string = environment.APPWRITE_DATABASE_ID;
+    BANK_COLLECTION_ID: string = environment.APPWRITE_BANK_COLLECTION_ID;
+
+  constructor(  private appwriteService: AppwriteService,
+      private plaidService: PlaidServiceService,
+      private dwollaService: DwollaServiceService) { }
+
+  async getBanks({ userId }: getBanks){
+    try {
+      const { database } = await this.appwriteService.createAdminClient();
+  
+      const banks = await database.listDocuments(
+        this.DATABASE_ID!,
+        this.BANK_COLLECTION_ID!,
+        [Query.equal("userId", [userId])]
+      );
+  
+      return parseStringify(banks.documents);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
 }
