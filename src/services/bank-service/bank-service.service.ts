46952a658b419a5db4c01c4c418d76886da5e0e8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-26 22:33:27 +0530

    chore: getAccounts fn

--- src/services/bank-service/bank-service.service.ts
+++ src/services/bank-service/bank-service.service.ts
@@ -3,7 +3,7 @@ import { environment } from '../../environments/environment.development';
 import { AppwriteService } from '../appwrite/appwrite.service';
 import { PlaidServiceService } from '../plaid-service/plaid-service.service';
 import { parseStringify } from '../../utils/util';
-import { getBank, getBankByAccountId, getBanks } from '../../types/types';
+import { getAccounts, getBank, getBankByAccountId, getBanks } from '../../types/types';
 import { DwollaServiceService } from '../dwolla-service/dwolla-service.service';
 import { Query } from 'appwrite';
 
@@ -71,5 +71,54 @@ export class BankServiceService {
       console.log(error);
     }
   };
+
+
+  async getAccounts({ userId }: getAccounts){
+    try {
+      // get banks from db
+      const banks = await this.getBanks({ userId });
+  
+      const accounts = await Promise.all(
+        banks?.map(async (bank: Bank) => {
+          // get each account info from plaid
+          const accountsResponse = await this.plaidService.plaidClient.accountsGet({
+            access_token: bank.accessToken,
+          });
+          const accountData = accountsResponse.data.accounts[0];
+  
+          // get institution info from plaid
+          const institution = await getInstitution({
+            institutionId: accountsResponse.data.item.institution_id!,
+          });
+  
+          const account = {
+            id: accountData.account_id,
+            availableBalance: accountData.balances.available!,
+            currentBalance: accountData.balances.current!,
+            institutionId: institution.institution_id,
+            name: accountData.name,
+            officialName: accountData.official_name,
+            mask: accountData.mask!,
+            type: accountData.type as string,
+            subtype: accountData.subtype! as string,
+            appwriteItemId: bank.$id,
+            sharaebleId: bank.shareableId,
+          };
+  
+          return account;
+        })
+      );
+  
+      const totalBanks = accounts.length;
+      const totalCurrentBalance = accounts.reduce((total, account) => {
+        return total + account.currentBalance;
+      }, 0);
+  
+      return parseStringify({ data: accounts, totalBanks, totalCurrentBalance });
+    } catch (error) {
+      console.error("An error occurred while getting the accounts:", error);
+    }
+  };
+
 
 }
