f8b4a768a9efaad82e984c696138ba0a1a791a03
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-26 23:05:27 +0530

    chore: Transaction type

--- src/types/types.ts
+++ src/types/types.ts
@@ -143,3 +143,21 @@ export type CategoryCount = {
   count: number;
   totalCount: number;
 };
+
+export type Transaction = {
+  id: string;
+  $id: string;
+  name: string;
+  paymentChannel: string;
+  type: string;
+  accountId: string;
+  amount: number;
+  pending: boolean;
+  category: string;
+  date: string;
+  image: string;
+  $createdAt: string;
+  channel: string;
+  senderBankId: string;
+  receiverBankId: string;
+};
