fb6f2eea3cb8c114313df58458c43e550d8c16a8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-01-30 21:34:09 +0530

    chore:  Category badge inital commit

---
+++ src/components/category-badge/category-badge.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-badge',
+  imports: [],
+  templateUrl: './category-badge.component.html',
+  styleUrl: './category-badge.component.scss'
+})
+export class CategoryBadgeComponent {
+
+}
