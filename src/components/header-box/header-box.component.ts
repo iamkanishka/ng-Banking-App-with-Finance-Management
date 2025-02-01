65bed8d55753c93b183c59ac8cf3a2e2b43f54d2
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-01 23:38:23 +0530

    chore: Update header-box.component.ts to include template and style inline

--- src/components/header-box/header-box.component.ts
+++ src/components/header-box/header-box.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header-box',
   imports: [],
-  templateUrl: './header-box.component.html',
-  styleUrl: './header-box.component.scss'
+  template: `<div class="header-box">
+    <h1 class="header-box-title">
+      {@title}
+      <%= if @type == "greeting" do %>
+        <span class="text-bankGradient">
+          &nbsp;{@user}
+        </span>
+      <% end %>
+    </h1>
+
+    <p class="header-box-subtext">{@subtext}</p>
+  </div>`,
+ 
 })
 export class HeaderBoxComponent {
 
