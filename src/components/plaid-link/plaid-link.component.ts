77610be3a0ba9e0bec2f762f952750482b0584b8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-01 23:44:53 +0530

    chore: Update inline template

--- src/components/plaid-link/plaid-link.component.ts
+++ src/components/plaid-link/plaid-link.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-plaid-link',
   imports: [],
-  templateUrl: './plaid-link.component.html',
-  styleUrl: './plaid-link.component.scss'
+  template: `<p>plaid-link works!</p>
+`,
+ 
 })
 export class PlaidLinkComponent {
 
