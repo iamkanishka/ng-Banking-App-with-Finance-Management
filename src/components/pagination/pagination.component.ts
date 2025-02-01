1dc6f44ddc2da76cc16c354b93b5bbcdd046b104
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-01 23:42:30 +0530

    refactor: Refactor pagination component to use template string instead of separate HTML file

--- src/components/pagination/pagination.component.ts
+++ src/components/pagination/pagination.component.ts
@@ -3,8 +3,44 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-pagination',
   imports: [],
-  templateUrl: './pagination.component.html',
-  styleUrl: './pagination.component.scss'
+  template: `
+  <div class="flex justify-between gap-3">
+    <.button
+      class="p-0 hover:bg-transparent"
+      phx-click="prev"
+      phx-target={@myself}
+      disabled={@page <= 1}
+    >
+      <div class="flex">
+        <img src="/images/arrow-left.svg" alt="arrow" width={20} height={20} class="mr-2" />
+        <p>Prev</p>
+      </div>
+    </.button>
+
+    <p class="text-14 flex items-center px-2">
+      {@page} / {@total_pages}
+    </p>
+
+    <.button
+      class="p-0 hover:bg-transparent"
+      phx-click="next"
+      phx-target={@myself}
+      disabled={@page >= @total_pages}
+    >
+      <div class="flex">
+        <p>Next</p>
+
+        <img
+          src="/images/arrow-left.svg"
+          alt="arrow"
+          width={20}
+          height={20}
+          class="ml-2 -scale-x-100"
+        />
+      </div>
+    </.button>
+  </div>`,
+ 
 })
 export class PaginationComponent {
 
