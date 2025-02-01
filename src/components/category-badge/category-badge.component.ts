f3d688b1becee7cb5e8a2936ef64bc29a8e8fdc8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-01 23:35:46 +0530

    chore: Removed template and style imports, replaced with inline template

--- src/components/category-badge/category-badge.component.ts
+++ src/components/category-badge/category-badge.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-category-badge',
   imports: [],
-  templateUrl: './category-badge.component.html',
-  styleUrl: './category-badge.component.scss'
+  template: '<div class="category-badge"></div>',
+ 
 })
 export class CategoryBadgeComponent {
 
