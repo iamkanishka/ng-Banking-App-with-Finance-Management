7e6ad8d62a5afcd7bd31c79cc40e7c6b3be9c90f
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-02 23:41:10 +0530

    feat: updated totalbox  component template and styles

--- src/components/total-balance-box/total-balance-box.component.ts
+++ src/components/total-balance-box/total-balance-box.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-total-balance-box',
   imports: [],
-  templateUrl: './total-balance-box.component.html',
-  styleUrl: './total-balance-box.component.scss'
+  template: `<p>total-balance-box works!</p>`,
+ 
 })
 export class TotalBalanceBoxComponent {
 
