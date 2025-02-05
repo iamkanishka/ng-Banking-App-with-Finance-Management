fbee4a1a58706a8b48fc1bc6fb9fe9f92f0d20ce
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-05 23:59:42 +0530

    updated intline temnplate

--- src/components/footer/footer.component.ts
+++ src/components/footer/footer.component.ts
@@ -4,6 +4,27 @@ import { Component } from '@angular/core';
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.component.html',
+  <footer class="footer">
+    <div class={"" <> if @type == "mobile", do: "footer_name-mobile", else: "footer_name"}>
+      <p class="text-xl font-bold text-gray-700">
+        {capitalize_first_letter(@user["first_name"])}
+      </p>
+    </div>
+
+    <div class={"" <> if @type == "mobile", do: "footer_email-mobile", else: "footer_email"}>
+      <h1 class="text-14 truncate text-gray-700 font-semibold">
+        {"#{@user["first_name"]} #{@user["last_name"]}"}
+      </h1>
+
+      <p class="text-14 truncate font-normal text-gray-600">
+        {String.slice("#{@user["email"]}", 0, 20)}...
+      </p>
+    </div>
+
+    <div class="footer_image" phx-click="handle_log_out" phx-target={@myself}>
+      <img src="/images/logout.svg" alt="logout" />
+    </div>
+  </footer>
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
