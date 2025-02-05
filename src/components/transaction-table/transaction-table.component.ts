7e62749ab12070e5dfac8fc6f1e08ad8ee9ca5a9
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-05 23:58:20 +0530

    chore: Added Input decorator for transactions array in TransactionTableComponent

--- src/components/transaction-table/transaction-table.component.ts
+++ src/components/transaction-table/transaction-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-transaction-table',
@@ -60,6 +60,8 @@ import { Component, OnInit } from '@angular/core';
  
 })
 export class TransactionTableComponent implements OnInit {
+  @Input() transactions!: [];
+
   constructor() {}
   ngOnInit(): void {}
 }
