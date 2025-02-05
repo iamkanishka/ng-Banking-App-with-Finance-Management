034933c7dca8f2571599e16f6158c4449ee79f10
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-05 23:07:34 +0530

    chore: Update pagination component  template changes

--- src/components/pagination/pagination.component.ts
+++ src/components/pagination/pagination.component.ts
@@ -1,37 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from 'angular/core';
 
-@Component({
+Component({
   selector: 'app-pagination',
   imports: [],
   template: `
   <div class="flex justify-between gap-3">
-    <.button
+    <button
       class="p-0 hover:bg-transparent"
-      phx-click="prev"
-      phx-target={@myself}
-      disabled={@page <= 1}
+      (click)="prev()"
+      [disabled]={page <= 1}
     >
       <div class="flex">
-        <img src="/images/arrow-left.svg" alt="arrow" width={20} height={20} class="mr-2" />
+        <img src="./assets/images/arrow-left.svg" alt="arrow" width="20" height="20" class="mr-2" />
         <p>Prev</p>
       </div>
-    </.button>
+    </button>
 
     <p class="text-14 flex items-center px-2">
-      {@page} / {@total_pages}
+      {{page}} / {{total_pages}}
     </p>
 
     <.button
       class="p-0 hover:bg-transparent"
-      phx-click="next"
-      phx-target={@myself}
-      disabled={@page >= @total_pages}
+      (click)="next"
+      disabled={page >= total_pages}
     >
       <div class="flex">
         <p>Next</p>
 
         <img
-          src="/images/arrow-left.svg"
+          src="
+          ./assets/images/arrow-left.svg"
           alt="arrow"
           width={20}
           height={20}
@@ -40,8 +39,5 @@ import { Component } from '@angular/core';
       </div>
     </.button>
   </div>`,
- 
-})
-export class PaginationComponent {
-
-}
+});
+export class PaginationComponent {}
