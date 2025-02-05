4e60c6d8e15e2b851978acfc3f35800380195acc
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-05 23:38:36 +0530

    chore: Update sidebar component  template changes

--- src/components/side-bar/side-bar.component.ts
+++ src/components/side-bar/side-bar.component.ts
@@ -1,51 +1,54 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
-  imports: [],
+  imports: [RouterLink],
   template: `
+ 
   
   <section class="sidebar">
     <nav class="flex flex-col gap-4">
-      <.link navigate={~p"/#{@key}"} class="mb-12 cursor-pointer flex items-center gap-2">
-        <img
-          src="/images/applogophxbank.png"
+      <a [routerLink]="['/key']" class="mb-12 cursor-pointer flex items-center gap-2">
+ 
+      <img
+          src="./assets/images/applogophxbank.png"
           alt="Horizon logo"
           width="64"
           class="size-[24px] max-xl:size-14"
           height="64"
         />
         <h1 class="sidebar-logo">Horizon</h1>
-      </.link>
+</a>
 
       <%= for %{label: label, imgURL: img_url, route: route}  <- SidebarLinks.get_sidebar_links() do %>
-        <% is_active = @current_url == route %>
-        <.link
-          navigate={"#{route}#{@key}"}
+        <% is_active = current_url == route %>
+        <a
+          [routerLink]="['/{route}{key}']"
           class={"sidebar-link " <> if is_active, do: "bg-bank-gradient", else: ""}
         >
           <div class="relative size-6">
             <img
-              src={img_url}
+              [src]={img_url}
               alt={label}
               class={"" <> if is_active, do: "brightness-[3] invert-0", else: ""}
             />
           </div>
 
-          <p class={"sidebar-label " <> if is_active, do: "!text-white", else: ""}>
+          <p class={  "sidebar-label " <> if is_active, do: "!text-white", else: ""}>
             {label}
           </p>
-        </.link>
+        </a>
       <% end %>
     </nav>
 
-    <%= if !@is_loading do %>
+    <%= if !is_loading do %>
       <.live_component
         module={PhoenixBankingAppWeb.CustomComponents.FooterLive}
         id="{:footer}"
         type="desktop"
-        user={@user}
-        key={@key}
+        user={user}
+        key={key}
       />
     <% end %>
   </section>
