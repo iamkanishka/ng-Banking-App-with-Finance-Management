8f3bbab4735c62f792ed3af815fe9973a506d805
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-05 23:53:21 +0530

    Added some fn declaration

--- src/components/bank-card/bank-card.component.ts
+++ src/components/bank-card/bank-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bank-card',
@@ -58,6 +58,20 @@ import { Component, OnInit } from '@angular/core';
    `
 })
 export class BankCardComponent implements OnInit {
+  @Input() account!: {};
+  @Input() userName!: String;
+
+
   constructor() {}
   ngOnInit(): void {}
+  bankTransactionHistory(){
+
+  }
+
+  format_amount(){
+
+  }
+
 }
+
+
