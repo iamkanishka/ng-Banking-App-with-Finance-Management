67d85b8b2d0d37d921a014ce0debb54616015ee2
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-06 23:32:12 +0530

    Added Catecory Template

--- src/components/category/category.component.ts
+++ src/components/category/category.component.ts
@@ -3,7 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-category',
   imports: [],
-  template: `<p>category works!</p>`,
+  template: `
+  
+  <div className={cn("gap-[18px] flex p-4 rounded-xl", bg)}>
+      <figure className={cn("flex-center size-10 rounded-full", circleBg)}>
+        <Image src={icon} width={20} height={20} alt={category.name} />
+      </figure>
+      <div className="flex w-full flex-1 flex-col gap-2">
+        <div className="text-14 flex justify-between">
+          <h2 className={cn("font-medium", main)}>{category.name}</h2>
+          <h3 className={cn("font-normal", count)}>{category.count}</h3>
+        </div>
+        <Progress
+          value={(category.count / category.totalCount) * 100}
+          className={cn("h-2 w-full", progressBg)}
+          indicatorClassName={cn("h-2 w-full", indicator)}
+        />
+      </div>
+    </div>
+
+  `,
  
 })
 export class CategoryComponent {
