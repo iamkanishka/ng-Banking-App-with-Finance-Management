0708834cda553026ece44bb4f2f11ec79d5cfded
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-06 23:55:09 +0530

    chore: Updated template

--- src/components/category/category.component.ts
+++ src/components/category/category.component.ts
@@ -7,18 +7,18 @@ import { Component } from '@angular/core';
   
   <div class={cn("gap-[18px] flex p-4 rounded-xl", bg)}>
       <figure class={cn("flex-center size-10 rounded-full", circleBg)}>
-        <Image src={icon} width={20} height={20} alt={category.name} />
+        <img src={icon} width={20} height=20} alt={category.name} />
       </figure>
       <div class="flex w-full flex-1 flex-col gap-2">
         <div class="text-14 flex justify-between">
-          <h2 class={cn("font-medium", main)}>{category.name}</h2>
-          <h3 class={cn("font-normal", count)}>{category.count}</h3>
+          <h2 class={cn("font-medium", main)}>{{category.name}}</h2>
+          <h3 class={cn("font-normal", count)}>{{category.count}}</h3>
         </div>
-        <Progress
+        <app-progress
           value={(category.count / category.totalCount) * 100}
           class={cn("h-2 w-full", progressBg)}
           indicatorclass={cn("h-2 w-full", indicator)}
-        />
+        /> <app-progress>
       </div>
     </div>
 
