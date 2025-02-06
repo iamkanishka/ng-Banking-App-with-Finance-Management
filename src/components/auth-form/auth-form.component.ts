b9f5b673bea8fc61dbf35957358b08ec79b843ba
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-06 23:10:25 +0530

    Auth forom component

---
+++ src/components/auth-form/auth-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-form',
+  imports: [],
+  template: `<p>auth-form works!</p>`,
+  styles: ``
+})
+export class AuthFormComponent {
+
+}
