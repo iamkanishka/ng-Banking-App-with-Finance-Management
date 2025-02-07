604bc5e8915bef497eefa0222bba78490631329b
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-07 21:46:23 +0530

    refactor: Refactor class

--- src/components/progress-bar/progress-bar.component.ts
+++ src/components/progress-bar/progress-bar.component.ts
@@ -2,17 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
+  standalone:true,
   imports: [],
   template: `
   
-  <ProgressPrimitive.Indicator
-      class={cn('h-full w-full flex-1 bg-primary transition-all', indicatorclass)}
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
+  <div class="relative h-4 w-full overflow-hidden rounded-full bg-secondary {{class}}">
+      <div
+        class="h-full w-full flex-1 bg-primary transition-all {{indicator_class}}"
+        style="transform: translateX(-#{100 - (@value || 0)}%);"
+      />
+    </div>
 
   `,
  
 })
 export class ProgressBarComponent {
+ 
+
 
 }
