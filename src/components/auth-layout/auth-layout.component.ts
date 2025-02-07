dce98e39cc30216fbd25006fdf093f93001c2a77
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-07 22:01:53 +0530

    AuthLayout Template

--- src/components/auth-layout/auth-layout.component.ts
+++ src/components/auth-layout/auth-layout.component.ts
@@ -3,9 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-auth-layout',
   imports: [],
-  template: '<p>auth-layout works!</p>',
+  template: `
+  
+  <main class="flex min-h-screen w-full justify-between font-inter">
+      <section class="flex-center size-full max-sm:px-6">
+
+      </section>
+    
+      </main>
+  `,
  
 })
 export class AuthLayoutComponent {
+
+  
 
 }
