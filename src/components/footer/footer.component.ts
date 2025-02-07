ac44d3705dab10c9db9fe7d2295cb5ca1a9cfbca
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-07 22:09:38 +0530

    refactor: Refactor class

--- src/components/footer/footer.component.ts
+++ src/components/footer/footer.component.ts
@@ -1,30 +1,31 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [NgClass],
   template: `
   
   
    <footer class="footer">
-    <div class={"" <> if @type == "mobile", do: "footer_name-mobile", else: "footer_name"}>
+    <div [ngClass]="{'' + type == 'mobile' ? 'footer_name-mobile': 'footer_name'}">
       <p class="text-xl font-bold text-gray-700">
-        {capitalize_first_letter(@user["first_name"])}
+        {{capitalize_first_letter(user["first_name"])}}
       </p>
     </div>
 
-    <div class={"" <> if @type == "mobile", do: "footer_email-mobile", else: "footer_email"}>
+    <div [ngClass]={"" <> if type == "mobile", do: "footer_email-mobile", else: "footer_email"}>
       <h1 class="text-14 truncate text-gray-700 font-semibold">
-        {"#{@user["first_name"]} #{@user["last_name"]}"}
+        {"#{user["first_name"]} #{user["last_name"]}"}
       </h1>
 
       <p class="text-14 truncate font-normal text-gray-600">
-        {String.slice("#{@user["email"]}", 0, 20)}...
+        {String.slice("#{user["email"]}", 0, 20)}...
       </p>
     </div>
 
-    <div class="footer_image" phx-click="handle_log_out" phx-target={@myself}>
-      <img src="/images/logout.svg" alt="logout" />
+    <div class="footer_image" (click)="handle_log_out()" >
+      <img src="./assets/images/logout.svg" alt="logout" />
     </div>
   </footer>
   `,
@@ -32,5 +33,17 @@ import { Component } from '@angular/core';
  
 })
 export class FooterComponent {
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
+ constructor() {}
+
+ capitalize_first_letter(firstname:string){
+
+ }
+
+ handle_log_out(){}
 
 }
