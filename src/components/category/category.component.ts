ab4a502b15b977b2e3d51889981c02023ad58c56
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-07 21:43:49 +0530

    refactor: Refactor styles

--- src/components/category/category.component.ts
+++ src/components/category/category.component.ts
@@ -1,30 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProgressBarComponent } from '../progress-bar/progress-bar.component';
 
 @Component({
   selector: 'app-category',
-  imports: [],
-  template: `
-  
-  <div class={cn("gap-[18px] flex p-4 rounded-xl", bg)}>
-      <figure class={cn("flex-center size-10 rounded-full", circleBg)}>
-        <img src={icon} width={20} height=20} alt={category.name} />
-      </figure>
-      <div class="flex w-full flex-1 flex-col gap-2">
-        <div class="text-14 flex justify-between">
-          <h2 class={cn("font-medium", main)}>{{category.name}}</h2>
-          <h3 class={cn("font-normal", count)}>{{category.count}}</h3>
-        </div>
-        <app-progress
-          value={(category.count / category.totalCount) * 100}
-          class={cn("h-2 w-full", progressBg)}
-          indicatorclass={cn("h-2 w-full", indicator)}
-        /> <app-progress>
-      </div>
-    </div>
+  imports: [ProgressBarComponent],
+  template: ` 
+  <div class="gap-[18px] flex p-4 rounded-xl  {{style.bg}} ">
 
-  `,
- 
+  <figure class="flex-center size-10 rounded-full {{style.circleBg}}">
+       <img [src]="icon" width="20" height="20" [alt]=category.name />
+       </figure>
+
+       <div class="flex w-full flex-1 flex-col gap-2">
+         <div class="text-14 flex justify-between">
+           <h2 class="font-medium {{style.main}}" >{{category.name}}</h2>
+           <h3 class="font-normal {{style.count}}" >{{category.count}}</h3>
+         </div>
+
+         <app-progress-bar
+           [value]="(category.count / category.totalCount) * 100"
+           class="h-2 w-full {{progressB}}"
+           indicatorclass="h-2 w-full {{indicator}}"
+         /> <app-progress-bar>
+         </div>
+
+  </div> `,
 })
 export class CategoryComponent {
 
+  style: object = {};
+  category: object = {};
+
+  constructor() {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
 }
