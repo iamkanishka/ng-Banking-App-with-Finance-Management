7b411778c98da0da4d000d539890aba2448c4b66
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-02-07 21:58:29 +0530

    Added the TotalBalanceBoxComponent template

--- src/components/total-balance-box/total-balance-box.component.ts
+++ src/components/total-balance-box/total-balance-box.component.ts
@@ -3,7 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-total-balance-box',
   imports: [],
-  template: `<p>total-balance-box works!</p>`,
+  template: `
+  
+  
+  <section class="total-balance flex flex-row gap-10">
+      <div class="total-balance-chart">
+     
+      </div>
+
+      <div class="flex flex-col gap-6 ms-16">
+        <h2 class="header-2">
+          Bank Accounts: {{total_banks}}
+        </h2>
+
+        <div class="flex flex-col gap-2">
+          <p class="total-balance-label">
+            Total Current Balance
+          </p>
+
+          <div class="total-balance-amount flex-center gap-2">
+            <div>
+              <span>{{total_current_balance}}</span>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+
+  
+  `,
  
 })
 export class TotalBalanceBoxComponent implements OnInit {
